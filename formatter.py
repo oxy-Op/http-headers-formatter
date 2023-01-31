6c57ef4bec382f6c04b608291fd4d59758b0e06e
oxy-Op <76278067+oxy-Op@users.noreply.github.com>
2023-01-31 21:20:17 +0530

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,7 +1,7 @@
 file  = open("data.txt","r",encoding = "utf-8")
 
 for i in file.readlines():
-    index_str = (i.find(":"))
-    a ="\"" + ((i[:index_str] + "\"" + i[index_str:]))
-    z = a[:index_str + 4] + "\"" + a[index_str + 4:]
-    print(z.strip() + "\"" + ",")
+    index_str = (i.find(":")) #finds ":" (without quotes) in string 
+    a ="\"" + ((i[:index_str] + "\"" + i[index_str:])) #adds " to both start and end, and to content before ":"
+    z = a[:index_str + 4] + "\"" + a[index_str + 4:] #adds " to content after ":"
+    print(z.strip() + "\"" + ",") #prints the formatted text, add new code if you want output in file
